f9dfc9d8c85c612dca6cd8d53d620da3c4031b88
pipel <pipel@DESKTOP-HIODS1C>
2016-06-01 18:56:38 -0700

    MicroServicio para turnos

--- TurnoMicroService/app.js
+++ TurnoMicroService/app.js
@@ -1,1 +1,56 @@
-﻿console.log('Hello world');
+﻿var TurnoLogicFactory = require('./services/Turno.js')
+
+function turnoPlugin(options) {
+    var turnoLogic;
+    
+    //Operaciones
+    this.add('role:turno,cmd:tomar', tomarTurno)
+    this.add('role:turno,cmd:cancelar', cancelarTurno)
+    this.add('role:turno,cmd:consultarPuesto', consultarPuesto)
+    
+    //Wrappers
+    this.wrap('role:turno', parseIntegers);
+    
+    
+    //Inicialización
+    this.add({ init: turnoPlugin }, init)
+    
+    
+    function tomarTurno(msg, respond) {
+        var result = turnoLogic.tomarTurno(msg.idCliente);
+        var out = { answer: result }
+        respond(null, out)
+    }
+    
+    function cancelarTurno(msg, respond) {
+        var result = turnoLogic.cancelarTurno(msg.idTurno);
+        var out = { answer: result }
+        respond(null, out)
+    }
+    
+    function consultarPuesto(msg, respond) {
+        var result = turnoLogic.consultarPuesto(msg.idTurno);
+        var out = { answer: result }
+        respond(null, out)
+    }
+    
+    function init(msg, respond) {
+        console.log("Iniciando microservicio...");
+        var dbTurno = msg.dbTurno;
+        var filasClient = msg.filasClient;
+        turnoLogic = new TurnoLogicFactory(db, filasClient);
+        respond();
+        console.log("Se inició con éxito!");
+    }
+    
+    function parseIntegers(msg, respond) {
+        msg.left = Number(msg.left).valueOf()
+        msg.right = Number(msg.right).valueOf()
+        this.prior(msg, respond)
+    }
+
+}
+
+require('seneca')()
+  .use(turnoPlugin, { dbTurno:null , filasClient: null })
+   .listen({ type: 'tcp', port: 1223, host: 'localhost', })
---
+++ TurnoMicroService/services/Turno.js
@@ -0,0 +1,43 @@
+﻿var logicaNegocio = function Turno(dbTurno, filasClient) {
+    return {
+        tomarTurno: function (idCliente) {
+            var fila = filasClient.obtenerFilaDisponible();
+            var cantidadPersonasFila = fila.Turnos.length;
+            var TiempoRestante = cantidadPersonasFila * 10;
+            var turno = {
+                codigoFila: fila.Id, 
+                Estado : "EnEspera",
+                Numero: cantidadPersonasFila  ,
+                TiempoRestante : TiempoRestante,
+                CodigoPago : idCliente
+            }
+
+            turno = dbTurno.create(turno);
+
+            return turno;
+        },
+        cancelarTurno: function (idTurno) {
+            var turnoACancelar = dbTurno.getTurnoById(idTurno);
+            if (turnoACancelar.Estado == "EnEspera") {
+                turnoACancelar.Estado = "Cancelado";
+                dbTurno.Update(turnoACancelar);
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        consultarPuesto: function (idTurno) {
+            var turno = dbTurno.getTurnoById(idTurno);
+            return turno;
+        }
+    }
+}
+
+//Estado:
+/*EnEspera
+ *EnAtencion
+ *Cancelado
+ *Finalizado*/
+
+module.exports = logicaNegocio;
