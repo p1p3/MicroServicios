0b6a99303946775e5d8fc16724df118504884b0c
pipel <pipel@DESKTOP-HIODS1C>
2016-06-01 18:25:27 -0700

    hkj

---
+++ WebServerIntegration/api.js
@@ -0,0 +1,27 @@
+﻿module.exports = function api(options) {
+    //http://localhost:3000/api/calculate/sumar?left=2&right=3 
+    //http://localhost:3000/api/calculate/multiplicar?left=2&right=3 
+    var valid_ops = { sumar: 'sum', multiplicar: 'product' }
+    
+    this.add('role:api,path:calculate', function (msg, respond) {
+        this.act('role:math', {
+            cmd: valid_ops[msg.operation],
+            left: msg.left,
+            right: msg.right,
+        }, respond)
+    })
+    
+    
+    this.add('init:api', function (msg, respond) {
+        this.act('role:web', {
+            use: {
+                prefix: '/api',
+                pin: 'role:api,path:*',
+                map: {
+                    calculate: { GET: true, suffix: '/:operation' }
+                }
+            }
+        }, respond)
+    })
+
+}
---
+++ WebServerIntegration/app.js
@@ -0,0 +1,9 @@
+﻿
+var seneca = require('seneca')()
+      .use('api')
+      .client({ type: 'tcp', port: 1223, host: 'localhost', })
+
+var app = require('express')()
+      .use(require('body-parser').json())
+      .use(seneca.export('web'))
+      .listen(3000)
